344ec8c11730fc2ca893e939dfc3cf414019e86e
iHamza7 <khanazam3324@gmail.com>
2023-03-10 20:43:13 -0800

    lib changed

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -88,7 +88,7 @@ class _HomePageState extends State<HomePage> {
                     alignment: Alignment.bottomCenter,
                     child: ElevatedButton.icon(
                       icon: const Icon(Icons.camera),
-                      label: const Text("Click"),
+                      label: const Text("Click here"),
                       onPressed: () async {
                         // If the returned path is not null, navigate
                         // to the DetailScreen
