1d7caa380f6fba1dc13cbefd2c396de27b6e5abb
iHamza7 <khanazam3324@gmail.com>
2023-03-10 20:40:34 -0800

    changed

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
+import 'details.dart';
 import 'main.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,13 +14,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  CameraController? _controller;
+  // Inside _CameraScreenState class
 
-  @override
-  void initState() {
-    super.initState();
-    _controller = CameraController(cameras[0], ResolutionPreset.medium);
-    _controller?.initialize().then((_) {
+  late final CameraController _controller;
+
+  void _initializeCamera() async {
+    final CameraController cameraController = CameraController(
+      cameras[0],
+      ResolutionPreset.high,
+    );
+    _controller = cameraController;
+
+    _controller.initialize().then((_) {
       if (!mounted) {
         return;
       }
@@ -26,17 +34,90 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  void initState() {
+    _initializeCamera();
+    super.initState();
+  }
+
+  @override
   void dispose() {
+    _controller.dispose();
     super.dispose();
-    _controller?.dispose();
+  }
+
+// Takes picture with the selected device camera, and
+// returns the image path
+  Future<String?> _takePicture() async {
+    if (!_controller.value.isInitialized) {
+      return null;
+    }
+
+    String? imagePath;
+
+    if (_controller.value.isTakingPicture) {
+      return null;
+    }
+
+    try {
+      // Turning off the camera flash
+      _controller.setFlashMode(FlashMode.off);
+      // Returns the image in cross-platform file abstraction
+      final XFile file = await _controller.takePicture();
+      // Retrieving the path
+      imagePath = file.path;
+    } on CameraException {
+      return null;
+    }
+
+    return imagePath;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ML Vision'),
+        title: const Text('Flutter MLKit Vision'),
       ),
+      body: _controller.value.isInitialized
+          ? Stack(
+              children: <Widget>[
+                CameraPreview(_controller),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Container(
+                    alignment: Alignment.bottomCenter,
+                    child: ElevatedButton.icon(
+                      icon: const Icon(Icons.camera),
+                      label: const Text("Click"),
+                      onPressed: () async {
+                        // If the returned path is not null, navigate
+                        // to the DetailScreen
+                        await _takePicture().then((String? path) {
+                          if (path != null) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DetailScreen(
+                                  imagePath: path,
+                                ),
+                              ),
+                            );
+                          } else {
+                            //print('Image path not found!');
+                          }
+                        });
+                      },
+                    ),
+                  ),
+                )
+              ],
+            )
+          : Container(
+              color: Colors.black,
+              child: const Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
     );
   }
 }
